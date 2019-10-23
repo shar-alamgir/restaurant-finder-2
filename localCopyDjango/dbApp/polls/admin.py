b6ff44bc914ae8ad45e11cd8d36e1a104a986179
Vincent Nguyen <vn4@illinois.edu>
2019-10-22 23:36:04 -0500

    started django backend

--- localCopyDjango/dbApp/polls/admin.py
+++ localCopyDjango/dbApp/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Restaurant, Menu, Hours
+admin.site.register(User)
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(Hours)
--- localCopyDjango/dbApp/polls/models.py
+++ localCopyDjango/dbApp/polls/models.py
@@ -1,3 +1,48 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class User(models.Model):
+    def __str__(self):
+        return self.user_name
+    user_name = models.CharField(max_length=50)
+    date_created = models.DateTimeField('date created')
+    location = models.CharField(max_length=50)
+    favorite_restaurant = models.CharField(max_length=50)
+
+class Restaurant(models.Model):
+    def __str__(self):
+        return self.restuarant_name
+    restuarant_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    price_tier = models.CharField(max_length=4)
+    rating = models.IntegerField(default=0)
+    # hours is a separate table
+    # cuisine tags, reviews is NoSQL
+
+class Menu(models.Model):
+    def __str__(self):
+        return self.restuarant_name
+    restuarant_name = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    food_type = models.CharField(max_length=20)
+    dietary_restrictions = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+
+class Hours(models.Model):
+    def __str__(self):
+        return self.restuarant_name
+    restuarant_name = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    m_open = models.CharField(max_length=8)
+    m_close = models.CharField(max_length=8)
+    t_open = models.CharField(max_length=8)
+    t_close = models.CharField(max_length=8)
+    w_open = models.CharField(max_length=8)
+    w_close = models.CharField(max_length=8)
+    th_open = models.CharField(max_length=8)
+    th_close = models.CharField(max_length=8)
+    f_open = models.CharField(max_length=8)
+    f_close = models.CharField(max_length=8)
+    sa_open = models.CharField(max_length=8)
+    sa_close = models.CharField(max_length=8)
+    su_open = models.CharField(max_length=8)
+    su_close = models.CharField(max_length=8)
