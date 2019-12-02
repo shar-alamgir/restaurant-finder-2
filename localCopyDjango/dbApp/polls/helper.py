5f37297aa3659685f949a511f60bce520cef9071
Vincent Nguyen <vn4@illinois.edu>
2019-12-02 15:45:06 -0600

    started recs

--- localCopyDjango/dbApp/polls/helper.py
+++ localCopyDjango/dbApp/polls/helper.py
@@ -98,6 +98,7 @@ def notValid(param, paramName):
 
 def getDistance(userAddr, restAddr):
     gmaps = googlemaps.Client(key='AIzaSyDeOyuHKTRGZr3YzOirYe5Wi1v5IN2ZhE4')
+    restAddr += ' Illinois'
     restObj = gmaps.geocode(restAddr)
     userObj = gmaps.geocode(userAddr)
     restaurantLat = restObj[0]['geometry']['location']['lat']
@@ -106,5 +107,68 @@ def getDistance(userAddr, restAddr):
     userLng = userObj[0]['geometry']['location']['lng']
     travelDistObj = gmaps.distance_matrix((userLat, userLng), (restaurantLat, restaurantLng))
     dist = travelDistObj['rows'][0]['elements'][0]['distance']['text']
-    pdb.set_trace()
+    #pdb.set_trace()
+    spaceIndex = dist.find(' ')
+    dist = dist[0:spaceIndex]
     return dist
+
+#recommendation helper
+def recommendRestaurant(conn, cuisines, price, rating, location, extra):
+    #perform nosql query to get list of all restaurant_name (s) that have at least one of the cuisine tags
+    sql = 'SELECT id, restaurant_name, location FROM polls_restaurant WHERE'
+    flag = False
+
+    if price is None:
+        sql = sql
+    else:
+        flag = True
+        numDollars = len(price)
+        neededUnderscores = numDollars - 1
+        underscores = "_" * neededUnderscores
+        priceString = "$" + underscores
+        newPriceString = "'" + priceString + "'"
+        sql = sql + ' price_tier LIKE ' + newPriceString
+
+    if rating is None:
+        sql = sql
+    else:
+        if flag is True:
+            sql = sql + ' AND rating >= ' + rating
+        else:
+            flag = True
+            sql = sql + ' rating >= ' + rating
+
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+
+    dropStatement = 'DROP TABLE IF EXISTS temp'
+    pdb.set_trace()
+    if location is None:
+        conn.close()
+        return rows
+    else:
+        cur.execute(dropStatement)
+        conn.commit()
+        newSql = "CREATE TABLE temp (id INTEGER, restaurant_name VARCHAR(30), location VARCHAR(30), distanceToUser float)"
+        cur.execute(newSql)
+        conn.commit()
+        pdb.set_trace()
+        for currRow in rows:
+            currDist = float(getDistance(location, currRow[2]))
+            alterSql = 'INSERT INTO temp (id, restaurant_name, location, distanceToUser) VALUES (?, ?, ?, ?)'
+            task = (currRow[0], currRow[1], currRow[2], currDist)
+            cur.execute(alterSql, task)
+            conn.commit()
+    selectRecs = 'SELECT * FROM temp ORDER BY distanceToUser ASC'
+    cur.execute(selectRecs)
+    recs = cur.fetchall()
+    pdb.set_trace()
+
+    cur.execute(dropStatement)
+    conn.commit()
+    conn.close()
+    return recs
+
+def sortRestaurants(userAddr, restLocs):
+    pass
--- localCopyDjango/dbApp/polls/views.py
+++ localCopyDjango/dbApp/polls/views.py
@@ -8,6 +8,8 @@ import cgi
 from . import helper
 import pdb
 
+database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
+
 def homeView(request):
     helper.getDistance('1301 W Springfield Ave, Champaign, IL', '603 S Wright St, Champaign, IL')
     return render(request, 'polls/homeView.html')
@@ -21,12 +23,12 @@ def allReviewsView(request):
     return render(request, 'polls/allReviewsView.html', context)
 
 def searchView(request):
+
     return render(request, 'polls/searchView.html')
 
 def searchResultsView(request):
     if request.method == 'POST':
         if request.POST.get('restaurant'):
-            database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
             conn = helper.create_connection(database)
             if conn is None:
                 return redirect('searchView')
@@ -34,8 +36,30 @@ def searchResultsView(request):
             result = helper.searchRestaurant(conn, searchString)
             context = {'result' : result}
             return render(request, 'polls/searchResultsView.html', context)
-        else:
-            return redirect('searchView')
+        else: #restaurant recommendation AF1
+            conn = helper.create_connection(database)
+            if conn is None:
+                return redirect('searchView')
+            #select all cuisines as one list
+            cuisines = request.POST.getlist('cuisine')
+            price = request.POST.get('price')
+            rating = request.POST.get('rating')
+            #only read the location if they checked the box
+            pdb.set_trace()
+            if request.POST.get('locationCheck'):
+                location = request.POST.get('location')
+            else:
+                location = None
+            #dine in vs carry out (might be hard to actually do since i couldn't parse that info from yelp)
+            extra = request.POST.get('extra')
+
+            #not sure if i can call this once for the entire cuisine list or if i should iterate through the list
+            result = helper.recommendRestaurant(conn, cuisines, price, rating, location, extra)
+            context = {'result' : result}
+            return render(request, 'polls/searchResultsView.html', context)
+
+            #return this for now until function works
+            # return redirect('searchView')
     return redirect('searchView')
 
 def userView(request, user_id):
@@ -69,7 +93,6 @@ def userView(request, user_id):
 
 def personalUserView(request, user_id):
     if request.method == 'POST':
-        database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
         conn = helper.create_connection(database)
         if conn is None:
             return 0
@@ -102,7 +125,6 @@ def allUsersView(request):
 
 def restaurantView(request, restaurant_id):
     if request.method == "POST":
-        database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
         conn = helper.create_connection(database)
         if conn is None:
             return 0
@@ -164,7 +186,6 @@ def allRestaurantView(request):
 def insertUserView(request):
     if request.method == 'POST':
         if request.POST.get('user_name'):
-            database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
             conn = helper.create_connection(database)
             if conn is None:
                 return redirect('homeView')
@@ -180,7 +201,6 @@ def insertUserView(request):
 def insertRestaurantView(request):
     if request.method == 'POST':
         if request.POST.get('restaurant_name'):
-            database = r"/Users/vincentnguyen/rf2/localCopyDjango/dbApp/db.sqlite3"
             conn = helper.create_connection(database)
             if conn is None:
                 return redirect('homeView')
