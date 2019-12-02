854747941f1d5166544ce6d40bf71d3bd272afed
mjcooney <marcus.cooney1@gmail.com>
2019-12-01 20:33:46 -0600

    Implemented mongodb and routers for reviews

--- localCopyDjango/dbApp/dbApp/routers.py
+++ localCopyDjango/dbApp/dbApp/routers.py
@@ -1,25 +1,28 @@
 class RevRouter:
-    route_table = "Review"
 
     def db_for_read(self, model, **hints):
-        if model._meta.db_table == "Review":
+        # print(model._meta.db_table)
+        if model._meta.db_table == "polls_reviews":
             return "reviews"
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.db_table == "Review":
+        # print(model._meta.db_table)
+        if model._meta.db_table == "polls_reviews":
+            print("Saving to review database")
             return "reviews"
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
+        # print(obj1._meta.db_table)
         if (
-            obj1._meta.db_table == "Review" or
-            obj2._meta.db_table == "Review"
+            obj1._meta.db_table == "polls_reviews" or
+            obj2._meta.db_table == "polls_reviews"
         ):
             return True
         return None
 
     def allow_migrate(self, db, db_table, model_name=None, **hints):
-        if db_table == "Review":
+        if db_table == "polls_reviews":
             return True
         return None
